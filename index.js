e23d6eee027f466b7ade9fbfc44197a759521329
vc743 <victorcontrerasvargas55@gmail.com>
2024-12-21 22:55:01 -0400

    Add blogs controller, create app.js to initialize middleware and routes, and update the index.js so that it only starts the server

--- index.js
+++ index.js
@@ -1,37 +1,5 @@
+const app = require("./app.js");
 const config = require("./utils/config.js");
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-const Blog = require("./models/blog");
-
-console.log("connecting to ", config.MONGODB_URI);
-
-mongoose
-  .connect(config.MONGODB_URI)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) =>
-    console.error("Error connecting to MongoDB", error.message)
-  );
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
